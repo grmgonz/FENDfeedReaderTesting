b5c65e717f4cd2bd605c5922da38ff1dfeec811b
Gabe Gonzales <grmgonz@gmail.com>
2016-08-28 11:54:20 -0600

    Initial Commit: Project Submission

---
+++ README.md
@@ -0,0 +1,28 @@
+# Project 6: Feed Reader Testing #
+
+
+## Run Project ##
+*Clone or download zip file and open index.html.
+*Open js/app.js and in the allFeeds array you can edit the feed sources to test functionality of tests made in jasmine/spec/feedreader.js
+*Open jasmine/spec/feedreader.js in order to edit feed reader tests.
+
+## About Project ##
+
+jasmine/spec/feedreader.js- *This is the spec file that Jasmine will read and contains
+all of the tests that will be run against your application.*
+
+### Tests ###
+
+#### RSS Feeds Definitions TEST SUITE ####
+*TEST 1: RSS Feeds are Defined
+*TEST 2: RSS Feed URLS are Defined
+*TEST 3: RSS Feeds Names Are Defined & Aren't Empty
+#### The Menu TEST SUITE ####
+*TEST 4: Menu Element is Hidden by Default
+*TEST 5: Menu Changes Visibility When it's Icon is Clicked
+#### Initial Entries TEST SUITE ####
+/*Done() is used as a callback to load feeds before tests are run */
+*TEST 6: Menu Changes Visibility When it's Icon is Clicked
+#### New Feed Selection TEST SUITE ####
+/*Done() is used as a callback to load feeds before tests are run */
+*TEST 7: Content Changes When New Feed is Loaded
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,104 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+ $(function() {
+
+
+/*RSS Feeds Definitions TEST SUITE*/
+describe('RSS Feeds.', function() {
+
+
+/*TEST 1: RSS Feeds are Defined*/
+    it('are defined.', function() {
+        expect(allFeeds).toBeDefined();
+        expect(allFeeds.length).not.toBe(0);
+        });
+
+
+/*TEST 2: RSS Feed URLS are Defined*/
+    it('have URLs defined.', function() {
+        expect(allFeeds).toBeDefined();
+        expect(allFeeds.length).not.toBe(0);
+        });
+
+
+/*TEST 3: RSS Feeds Names Are Defined & Aren't Empty*/
+    it('have a name defined and names are not empty.', function() {
+        allFeeds.forEach(function(feed) {
+        expect(feed.name).toBeDefined();
+        expect(feed.name).not.toBe(true);
+        });
+    });
+});
+
+
+/*The Menu TEST SUITE*/
+describe('The Menu', function() {
+    var body = $('body'),
+    menuIcon = $('.menu-icon-link');
+
+
+/*TEST 4: Menu Element is Hidden by Default */
+    it('is hidden by default.', function() {
+        expect(body.hasClass('menu-hidden')).toBe(true);
+        });
+
+
+/*TEST 5: Menu Changes Visibility When it's Icon is Clicked*/
+    it('changes visibility when icon is clicked.', function() {
+        menuIcon.click();
+        expect(body.hasClass('menu-hidden')).toBe(false);
+
+        menuIcon.click();
+        expect(body.hasClass('menu-hidden')).toBe(true);
+        });
+    });
+
+
+/*Initial Entries TEST SUITE*/
+/*Done() is used as a callback to load feeds before tests are run */
+describe('Initial Entries', function() {
+    beforeEach(function(done) {
+        loadFeed(0, function() {
+        done();
+        });
+    });
+
+
+/*TEST 6: Menu Changes Visibility When it's Icon is Clicked*/
+    it('have at least one entry.', function(done) {
+        var entries = $('.feed .entry').length;
+        expect(entries).toBeGreaterThan(0);
+        done();
+        });
+    });
+
+
+/*New Feed Selection TEST SUITE*/
+/*Done() is used as a callback to load feeds before tests are run */
+describe('New Feed Selection', function() {
+    var feed;
+
+
+/*TEST 7: Content Changes When New Feed is Loaded*/
+    beforeEach(function(done) {
+        feed = $('.feed').html();
+        loadFeed(1, function() {
+        done();
+        });
+    });
+
+    it('changes the content displayed.', function(done) {
+        var changed = $('.feed').html();
+        expect(feed).not.toBe(changed);
+        done();
+        });
+    });
+}());
